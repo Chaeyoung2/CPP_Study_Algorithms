a7bb283050ca46960f488ee9f477bb55024e49a4
Chae Young Son <43426737+Chaeyoung2@users.noreply.github.com>
2022-03-27 17:27:37 +0900

    Create A27.cpp
    이진 탐색, incomplete

---
+++ ThisIsCodingTest/Project1/Project1/A27.cpp
@@ -0,0 +1,20 @@
+// chap15. 이진 탐색. 정렬된 배열에서 특정 수의 개수 구하기.
+// 다시 풀어~
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, x;
+vector<int> arr;
+
+int main() {
+	cin >> n >> x;
+	for (int i = 0; i < n; i++) {
+		int val;
+		cin >> val;
+		arr.push_back(val);
+	}
+
+}
