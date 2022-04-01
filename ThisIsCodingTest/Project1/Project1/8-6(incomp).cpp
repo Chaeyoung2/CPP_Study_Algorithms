5c7a81875d749f1d86d5cba68429040fb2cc9af5
Chae Young Son <43426737+Chaeyoung2@users.noreply.github.com>
2022-04-01 16:24:13 +0900

    create 8-6.cpp
    Dynamic programming, incomplete

---
+++ ThisIsCodingTest/Project1/Project1/8-6(incomp).cpp
@@ -0,0 +1,23 @@
+// 8-6. 개미 전사
+// 다시 풀기
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+using namespace std;
+
+int dp[100];
+int n;
+vector<int> arr;
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		arr.push_back(x);
+	}
+
+	// dynamic programming (bottom up)
+}
+
