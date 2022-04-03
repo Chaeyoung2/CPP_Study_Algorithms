d0af5fc3e9ad4299add4ddbb7a859fb6fe6bfbea
Chae Young Son <43426737+Chaeyoung2@users.noreply.github.com>
2022-04-03 22:47:18 +0900

    Re-solve 8-6(incomp).cpp
    Dynamic Programming, Re-solve

--- ThisIsCodingTest/Project1/Project1/8-6(incomp).cpp
+++ ThisIsCodingTest/Project1/Project1/8-6(incomp).cpp
@@ -1,5 +1,5 @@
 // 8-6. 개미 전사
-// 다시 풀기
+// 다시 풀기 (4/3 다시 풀었으나 틀림, ** 확인)
 
 #include <iostream>
 #include <vector>
@@ -19,5 +19,19 @@ int main() {
 	}
 
 	// dynamic programming (bottom up)
+	dp[0] = arr[0];
+	// dp[1] = arr[1]; // ** 틀렸어!!!!!!!!!!!!!
+	dp[1] = max(arr[0], arr[1]);
+
+	for (int i = 2; i < n; i++) {
+		if (dp[i - 1] < dp[i - 2] + arr[i]) {
+			dp[i] = dp[i - 2] + arr[i];
+		}
+		else {
+			dp[i] = dp[i - 1];
+		}
+	}
+	
+	cout << dp[n - 1] << "\n";
 }
 
