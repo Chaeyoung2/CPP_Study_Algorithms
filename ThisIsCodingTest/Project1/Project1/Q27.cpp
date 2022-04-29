4b578dce1e610da48e9579211c36fbfb8315e594
Chae Young Son <43426737+Chaeyoung2@users.noreply.github.com>
2022-04-29 20:07:41 +0900

    답안
    upper_bound와 lower_bound를 활용한다면 더 깔끔한 코드가 될 것

--- ThisIsCodingTest/Project1/Project1/Q27.cpp
+++ ThisIsCodingTest/Project1/Project1/Q27.cpp
@@ -16,33 +16,14 @@ int main()
 		cin >> v[i];
 	}
 
-	int b, e, mid;
-	b = 0;
-	mid = n / 2;
-	e = n - 1;
-	int result = -1;
- 	while (e != b + 1) {
-		if (v[mid] == x) {
-			int b_temp = mid;
-			int e_temp = mid;
-			while (v[b_temp] == x) {
-				b_temp--;
-			}
-			while (v[e_temp] == x) {
-				e_temp++;
-			}
-			result = e_temp - b_temp - 1;
-			break;
-  		}
-		else if (binary_search(&v[b], &v[mid], x)) {
-			e = mid;
-		}
-		else {
-			b = mid;
-		}
-		mid = (b + e) / 2;
+	vector<int>::iterator u_index = upper_bound(v.begin(), v.end(), x);
+	vector<int>::iterator l_index = lower_bound(v.begin(), v.end(), x);
+
+	if (u_index == l_index) {
+		cout << -1 << "\n";
 	}
-	cout << result << "\n";
+	else
+		cout << u_index - l_index << "\n";
 }
 
 
