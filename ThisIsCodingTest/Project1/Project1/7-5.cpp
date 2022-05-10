ac8c7966b1ef8a733a402d2020a8d46b02958164
Chae Young Son <43426737+Chaeyoung2@users.noreply.github.com>
2022-05-10 21:38:59 +0900

    Update 7-5.cpp
    BinarySearch

--- ThisIsCodingTest/Project1/Project1/7-5.cpp
+++ ThisIsCodingTest/Project1/Project1/7-5.cpp
@@ -9,66 +9,31 @@ using namespace std;
 
 int n, m;
 
-int binary_search_while(vector<int>& v, int target, int start, int end) {
-	while (start < end) {
-		int mid = (start + end) * 0.5;
-		if (v[mid] == target) {
-			return mid;
-		}
-		else if (v[mid] < target) {
-			start = mid + 1;
-		}
-		else {
-			end = mid - 1;
-		}
-	}
-}
-
-int binary_search(vector<int>& v, int target, int start, int end) {
-	if (start >= end) return -1;
-	int mid = (start + end) * 0.5;
-	if (target == v[mid])
-		return mid;
-	else if (target > v[mid]) {
-		return binary_search(v, target, mid + 1, end);
-	}
-	else {
-		return binary_search(v, target, start, mid - 1);
-	}
-}
-
 int main() {
-
-	vector<int> ns;
-	vector<int> ms;
-
-
+	// ют╥б
 	cin >> n;
+	vector<int> ns(n);
 	for (int i = 0; i < n; i++) {
-		int val;
-		cin >> val;
-		ns.push_back(val);
+		cin >> ns[i];
 	}
-
 	cin >> m;
+	vector<int> ms(m);
 	for (int i = 0; i < m; i++) {
-		int val;
-		cin >> val;
-		ms.push_back(val);
+		cin >> ms[i];
 	}
 
 	sort(ns.begin(), ns.end());
+	sort(ms.begin(), ms.end());
 
-	int search_cnt = 0;
-	for (int i = 0; i < m; i++) {
-		if (-1 != binary_search(ns, ms[i], 0, n-1))
-			cout << "yes ";
-		else
-			cout << "no ";
+	for (int& m : ms) {
+		if (binary_search(ns.begin(), ns.end(), m)) {
+			cout << "yes";
+		}
+		else {
+			cout << "no";
+		}
+		cout << " ";
 	}
-	cout << '\n';
-
-
 }
 
 
