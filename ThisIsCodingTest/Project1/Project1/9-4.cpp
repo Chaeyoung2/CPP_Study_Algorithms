4294bc0041b8ab65fb06c4c7b4476e443282df79
Chae Young Son <43426737+Chaeyoung2@users.noreply.github.com>
2022-05-16 14:50:00 +0900

    Update 9-4.cpp
    FloydWarshall, 암기됨

--- ThisIsCodingTest/Project1/Project1/9-4.cpp
+++ ThisIsCodingTest/Project1/Project1/9-4.cpp
@@ -1,6 +1,8 @@
 // 9-4. 미래 도시
 // (5/11 답 보고 풀었음)
-// 
+// (5/14 알고리즘 아이디어에서 막혀서 답 보고 풀었음)
+// (5/16 최근에 계속 풀었어서 그런가 암기됨. 그냥 풀었음)
+
 // 1-N
 // 1에 위치, X에 방문해 물건 판매
 // 연결된 2개 회사는 양방향 이동 가능
@@ -9,48 +11,53 @@
 // 1-K-X 을 최단 거리로 이동할 때의 최소 시간은?
 
 // 입력 예시 1: 5 7 1 2 1 3 1 4 2 4 3 4 3 5 4 5 4 5 // 출력 예시 1: 3
-#include <iostream>
-using namespace std;
-#define INF 1e9
+// 입력 : 4 2 1 3 2 4 3 4 // 출력 : -1
 
-int n, m, x, k;
-int d[101][101];
+#include <iostream>
+#define INF 1e9
+using namespace std;
+
+int graph[101][101];
+int n, m;
+
+void floyd() {
+	for (int k = 1; k < n + 1; k++) {
+		for (int a = 1; a < n + 1; a++) {
+			for (int b = 1; b < n + 1; b++) {
+				graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b]);
+			}
+		}
+	}
+}
+
 int main()
 {
 	cin >> n >> m;
-	for (int i = 0; i < 101; i++) {
-		for (int j = 0; j < 101; j++) {
-			d[i][j] = INF;
-		}
-	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (i == j) d[i][j] = 0;
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < n + 1; j++) {
+			if (i == j)
+				graph[i][j] = 0;
+			else
+				graph[i][j] = INF;
 		}
 	}
 
 	for (int i = 0; i < m; i++) {
 		int val1, val2;
 		cin >> val1 >> val2;
-		d[val1][val2] = 1;
-		d[val2][val1] = 1;
+		graph[val1][val2] = 1;
+		graph[val2][val1] = 1;
 	}
 
+	int x, k;
 	cin >> x >> k;
 
-	for (int k = 1; k <= n; k++) {
-		for (int a = 1; a <= n; a++) {
-			for (int b = 1; b <= n; b++) {
-				d[a][b] = min(d[a][b], d[a][k] + d[k][b]);
-			}
-		}
-	}
+	floyd();
 
-	int dist = d[1][k] + d[k][x];
-	if (dist >= 1e9) {
+	int dist = graph[1][k] + graph[k][x];
+	if (dist >= INF)
 		cout << -1 << "\n";
-	}
 	else
 		cout << dist << "\n";
 }
