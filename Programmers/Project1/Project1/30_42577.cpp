abcc697259e7693b00e58b7dc5ccb943e6fe15de
Chae Young Son <43426737+Chaeyoung2@users.noreply.github.com>
2022-05-19 15:06:46 +0900

    Update 30_42577.cpp
    Binary_search로 재풀이

--- Programmers/Project1/Project1/30_42577.cpp
+++ Programmers/Project1/Project1/30_42577.cpp
@@ -1,32 +1,24 @@
 // 한 번호가 다른 번호의 접두어인지?
 // (5/18) X 아이디어 힌트 받아서 풀었음. 다시 풀 것
+// (5/19) X 이진 탐색 전에 정렬하는거 왜 까먹어~??
 
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <map>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
-
-    // 119-976, 119-119, 
-    // 123-456, 123-789
-    // 12-12, 12-12, 12-56, 12-88
-
-    map<string, int> m;
+    sort(phone_book.begin(), phone_book.end());
     for (string& s : phone_book) {
-        m.insert({ s, 0 });
-    }
-
-    for (auto& i : m) {
-        string target = i.first;
-        int target_len = target.length();
-        for (int i = 1; i < target_len; i++) {
-            string finding = target.substr(0, i);
-            if (m.end() != m.find(finding)) 
+        int st_len = s.length();
+        string des = s;
+        for (int i = 1; i < st_len; i++) {
+            // des의 접두어 찾기
+            if (binary_search(phone_book.begin(), phone_book.end(), des.substr(0, i))) {
                 return false;
+            }
         }
     }
 
