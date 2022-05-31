22d77ece31bdabcd4e787fff63d2869ca98014ee
Chae Young Son <43426737+Chaeyoung2@users.noreply.github.com>
2022-05-31 17:33:32 +0900

    Update 5-10.cpp
    dfs, O

--- ThisIsCodingTest/Project1/Project1/5-10.cpp
+++ ThisIsCodingTest/Project1/Project1/5-10.cpp
@@ -1,50 +1,52 @@
 // 5-10. 음료수 얼려 먹기
+// 4 5 00110 00011 11111 00000
+// 15 14 00000111100000 11111101111110 11011101101110 11011101100000 11011111111111 11011111111100 11000000011111 01111111111111 00000000011111 01111111111000 00011111111000 00000001111000 11111111110011 11100011111111 11100011111111
 // 3/30 - O
 // 4/20 - △ (36번줄~)
+// 5/31 - O
+
 #include <iostream>
+#include <string>
 using namespace std;
-int n, m;
+
 int arr[1001][1001];
-int result = 0;
+bool visited[1001][1001];
+int n, m;
 
-bool dfs(int h, int w) {
-	if (arr[h][w] == 1) return false;
-	arr[h][w] = 1;
+bool dfs(int row, int col){
+	if (row < 0 || row >= n || col < 0 || col >= m) return false;
+	if (arr[row][col] == 1) return false;
+	if (visited[row][col] == true) return false;
+	visited[row][col] = true;
 
-	if (h - 1 >= 0)
-		dfs(h - 1, w);
-
-	if(w + 1 < m) 
-		dfs(h, w + 1);
-
-	if(h + 1 < n)
-		dfs(h + 1, w);
-
-	if(w - 1 >= 0) 
-		dfs(h, w - 1);
+	dfs(row - 1, col);
+	dfs(row + 1, col);
+	dfs(row, col - 1);
+	dfs(row, col + 1);
 
 	return true;
-
 }
 
-int main()
-{
-	// setting
+int main() {
 	cin >> n >> m;
+
+	string s;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			scanf_s("%1d", &arr[i][j]);
+		cin >> s;
+		int size = s.size();
+		for (int j = 0; j < size; j++) {
+			arr[i][j] = (s[j] - '0');
 		}
 	}
 
-	// solution
+	int cnt = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if(dfs(i, j))
-				result++;
+			if (true == dfs(i, j)) {
+				cnt++;
+			}
 		}
 	}
 
-	cout << result << "\n";
+	cout << cnt << "\n";
 }
-
