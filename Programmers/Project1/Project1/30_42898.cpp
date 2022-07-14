75bd6a574fa6c738f6a031737e98dc2b4aab9f1c
Chae Young Son <43426737+Chaeyoung2@users.noreply.github.com>
2022-07-14 10:56:15 +0900

    Update 30_42898.cpp
    dynamic programming, O

--- Programmers/Project1/Project1/30_42898.cpp
+++ Programmers/Project1/Project1/30_42898.cpp
@@ -1,42 +1,51 @@
 // 프로그래머스 고득점kit 동적계획법 - 등굣길
-// (7/11 - ?) 채점이 안됨
+// (7/11 - X) 틀림
+// (7/12 - X)
+// (7/14 - O)
 #include <string>
 #include <vector>
 
 using namespace std;
 
-
 int dp[101][101];
-
-void go(int x, int y, vector<vector<int>>& p) {
-    if (x == 1 && y == 1) return;
-    for (const vector<int>& v : p) {
-        if (v[0] == x && v[1] == y) return;
-    }
-    dp[x][y] = min(dp[x][y], dp[x-1][y] + 1);
-    dp[x][y] = min(dp[x][y], dp[x][y-1] + 1);
-}
-
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
+    
     for (int i = 0; i < 101; i++) {
-        for (int j = 0; j < 101; j++) {
-            dp[i][j] = 1e9;
-        }
-    }
-    dp[1][1] = 0;
-
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            go(i, j, puddles);
-        }
+        dp[0][i] = -1;
+        dp[i][0] = -1;
     }
 
-    answer = dp[m][n] - 1;
+    dp[1][1] = 1;
+
+    for (vector<int>& v : puddles) {
+        dp[v[1]][v[0]] = -1;
+    }
+
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (i == 1 && j == 1) continue; // 시작 좌표
+            if (dp[i][j] == -1) continue; // 웅덩이
+            if (dp[i][j - 1] == -1 && dp[i - 1][j] == -1) { // 경우4
+                dp[i][j] = 0;
+            }
+            else if (dp[i][j - 1] == -1) { // 경우2
+                dp[i][j] = dp[i - 1][j] % 1'000'000'007;
+            }
+            else if (dp[i - 1][j] == -1) { // 경우3
+                dp[i][j] = dp[i][j - 1] % 1'000'000'007;
+            } 
+            else { // 경우4
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1'000'000'007;
+            }
+        }
+    }
+    
+    answer = dp[n][m] ;
 
     return answer;
 }
-
 int main() {
     vector<vector<int>> v = { {2,2} };
     solution(4, 3, v);
