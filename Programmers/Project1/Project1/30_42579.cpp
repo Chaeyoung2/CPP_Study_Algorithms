7b4e016edd6b97700b671fd07d18aa1c4a7526a3
ChaeYoung <43426737+Chaeyoung2@users.noreply.github.com>
2022-07-15 22:39:32 +0900

    Update 30_42579.cpp
    hash, O

--- Programmers/Project1/Project1/30_42579.cpp
+++ Programmers/Project1/Project1/30_42579.cpp
@@ -1,35 +1,62 @@
+// 프로그래머스 고득점 kit - 베스트 앨범
+// (7/15 - O)
+
 #include <string>
-#include <vector>
 #include <algorithm>
+#include <vector>
 #include <map>
 
 using namespace std;
 
+// 베스트 앨범에 들어갈 노래의 고유번호를 순서대로 리턴!
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
+    int song_cnt = genres.size();
 
-    
-    map<string, int> genre_cnt;// 어떤 장르가 가장 많이 play 되었는지 찾기
-    int size = genres.size();
-    for (int i = 0; i < size; i++) {
-        genre_cnt[genres[i]] += plays[i];
-    }    
-    string max_g = max_element(genre_cnt.begin(), genre_cnt.end(), [](const pair<string,int>& l, const pair<string,int>& r) {
-        return l.second < r.second;
-        })->first;
-    
+    // 1. 가장 많이 플레이된 장르는?
+    map<string, int> m1;
+    for (int i = 0; i < song_cnt; i++) {
+        m1[genres[i]] += plays[i];
+    }
+    int genres_cnt = m1.size();
 
-    // 많이 play 된 장르에서 두 곡 뽑기
-    map<int, int> m; // first:고유 번호, second:play 횟수
-    for (int i = 0; i < size; i++) {
-        if (genres[i] == max_g) {
-            m[i] = plays[i];
+    int while_cnt = 0;
+    while (!m1.empty()) {
+        vector < pair<string, int >> v1(m1.begin(), m1.end());
+        sort(v1.begin(), v1.end(), [](const pair<string, int>& p1, const pair<string, int>& p2) {
+            return p1.second > p2.second;
+            });
+        string maxplay_genre = v1[0].first;
+        m1.erase(maxplay_genre);
+
+        // 2. 가장 많이 플레이된 장르 중에서 가장 많이 플레이된 곡 두 개를 뽑아서 고유번호를 push
+        map<int, int> m2; // first 고유번호, second 플레이 횟수
+        for (int i = 0; i < song_cnt; i++) {
+            if (genres[i] == maxplay_genre) {
+                m2[i] = plays[i];
+            }
+        }
+        vector<pair<int, int>> v2(m2.begin(), m2.end());
+        sort(v2.begin(), v2.end(), [](const pair<int, int>& p1, const pair<int, int>& p2) {
+            if (p1.second == p2.second) {
+                p1.first < p2.first;
+            }
+            else {
+                return p1.second > p2.second;
+            }});
+        int song_cnt = m2.size();
+        for (int i = 0; i < song_cnt; i++) {
+            if (i >= 2) break;
+            answer.push_back(v2[i].first);
         }
     }
+
+
 
     return answer;
 }
 
 int main() {
-    solution({ "classic", "pop", "classic", "classic", "pop" }, { 500, 600, 150, 800, 2500 });
+    solution({ "classic", "pop", "classic", "classic", "pop" },
+        { 500, 600, 150, 800, 2500 });
 }
